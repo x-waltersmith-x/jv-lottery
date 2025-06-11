8e9a1500010cf19d6bfbb8eb7f3b7705188de649
Volodymyr Kovalov <vkovalov@Volodymyrs-Mac-mini.local>
2025-06-11 11:30:58 +0200

    Create Lottery object out of for loop

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,8 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        Lottery lottery = new Lottery();
+
         for (int i = 0; i <= 2; i++) {
-            Lottery lottery = new Lottery();
             Ball randomBall = lottery.getRandomBall();
 
             System.out.println(randomBall.toString());
